3b2e5d1cf9555666ff9e97c85d7cb6f7f89b6720
kamarali <kamarali.dukandar@gmail.com>
2016-08-30 13:23:04 +0530

    Add files via upload

---
+++ Data/ManageSystemParameter/ManageSystemParameterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iata.IS.Data.ManageSystemParameter
+{
+    public class ManageSystemParameterModel
+    {
+        public string SystemId { get; set; }
+
+        public string Systemstring { get; set; }
+
+        public string  Systemparameterkey { get; set; }
+
+        public string  SystemParameterValue { get; set; }
+
+        public string Id { get; set; }
+    }
+}
