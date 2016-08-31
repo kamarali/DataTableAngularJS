48814f830c408228bc0a2db7c9f60663dc67a1a1
kamarali <kamarali.dukandar@gmail.com>
2016-08-31 09:58:53 +0530

    Add files via upload

---
+++ Iata.IS.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Iata.IS.Web.Controllers;
+
+namespace Iata.IS.Web.Tests.Controllers
+{
+  [TestClass]
+  public class HomeControllerTest
+  {
+   
+    [TestMethod]
+    public void Index()
+    {
+      //// Arrange
+      //var controller = new HomeController();
+
+      //// Act
+      //var result = controller.Index() as ViewResult;
+
+      //// Assert
+      //Assert.IsNotNull(result);
+      //var viewData = result.ViewData;
+      //Assert.AreEqual("Welcome to ASP.NET MVC!", viewData["Message"]);
+    }
+
+    [TestMethod]
+    public void About()
+    {
+      // Arrange
+      //var controller = new HomeController();
+
+      //// Act
+      //var result = controller.About() as ViewResult;
+
+      //// Assert
+      //Assert.IsNotNull(result);
+    }
+  }
+}
